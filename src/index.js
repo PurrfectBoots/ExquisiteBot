9f2f85cbc873742e577e8ac331b0429c1c9fce90
Nat1001 <nathan.gm.38@gmail.com>
2023-05-30 19:12:04 +0200

    ♥ KeepAlive

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ const { Client, Collection } = require("discord.js");
 const { Colors, BetterConsoleLogger } = require("discord.js-v14-helper");
 const fs = require("fs");
 const config = require("./config/main");
-const https = require('https');
-const express = require('express');
-
+const https = require("https");
+const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-    console.log(Date.now() + " Ping Received");
-    response.sendStatus(200);
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
 });
-
 app.listen(process.env.PORT);
+setInterval(() => {
+  https.get(`https://discaventures-keepalive-40yd.onrender.com`);
+}, 600000);
+
+process.on("SIGTERM", () => {
+  console.log("Received SIGTERM. Exiting...");
+  process.exit(0);
+});
 
 const client = new Client(config.client.constructor);
 
